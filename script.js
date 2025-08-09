725ebe3cc6adc38056d384c29a14a6d55bbd20e0
Maha561 <mahalakshmianandan04@gmail.com>
2025-08-09 19:19:26 +0530

    added

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 
 
+const api = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0560e8ee97daa00b225fc0f2a28c37b7";
+const btn = document.getElementById("getBtn");
+const input = document.getElementById("CityInput");
+const output=document.getElementById("result");
+
+btn.addEventListener("click",function(){
+
+
+});
